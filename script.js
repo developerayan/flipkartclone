36c7b5632037053d13db8e0077df5f1ee77b400c
developerayan <151773122+developerayan@users.noreply.github.com>
2023-11-22 09:36:45 -0800

    added

---
+++ script.js
@@ -0,0 +1,65 @@
+console.log("FlipKart Loves Web")
+
+
+document.getElementById('btn');
+
+btn.addEventListener('click', ()=>{
+    document.getElementById('image1')
+
+    if(image1.style.visibility === "hidden"){
+        image1.style.visibility = "visible"
+    }
+    else{
+        image1.style.visibility = "hidden"
+    }
+
+    document.getElementById('image2')
+
+    if(image2.style.visibility === "visible"){
+        image2.style.visibility = "hidden"
+    }
+    else{
+        image2.style.visibility = "visible"
+    }
+
+    document.getElementById('tv')
+
+        if(tv.style.visibility === "hidden"){
+            tv.style.visibility = "visible"
+        }
+        else{
+            tv.style.visibility = "hidden"
+        }
+
+        document.getElementById('infinix')
+
+        if(infinix.style.visibility === "visible"){
+            infinix.style.visibility = "hidden"
+        }
+        else{
+            infinix.style.visibility = "visible"
+        }
+        
+        document.getElementById('text')
+
+        if(text.style.visibility === "visible"){
+            text.style.visibility = "hidden"
+        }
+        else{
+            text.style.visibility = "visible"
+        }
+        
+})
+
+document.getElementById('menu');
+
+function come() {
+    document.getElementById('itemcon')
+
+    if(itemcon.style.visibility === 'visible'){
+        itemcon.style.visibility = 'hidden'
+    }
+    else{
+        itemcon.style.visibility = 'visible'
+    }
+}
